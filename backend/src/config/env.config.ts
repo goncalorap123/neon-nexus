3eda49532fb023fb99b35c44fbf73a7ee8766ac1
goncalorap123 <goncalo.rap123@gmail.com>
2026-03-29 18:34:47 +0900

    configs

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 export interface EnvConfig {
   PRIVY_APP_ID: string;
